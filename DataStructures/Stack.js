673778169a0a6381f4599bed1e33c1435ed2757a
Abdelrahman Mostafa <Abdelraahmanmostafa@gmail.com>
2022-04-24 21:10:01 +0200

    Create Stack.js

---
+++ DataStructures/Stack.js
@@ -0,0 +1,31 @@
+class Stack {
+    constructor() { 
+        this.arr = []
+    }
+    
+    push(value) {
+        this.arr.push(value)
+        this.length++
+    }
+    
+    pop() {
+        this.arr.pop()
+        this.length--
+    }
+    
+    peek() {
+        return this.arr[this.arr.length-1]
+    }
+    print() {
+        console.log(this.arr)
+    }
+    
+};
+
+const myStack = new Stack()
+myStack.push(5)
+myStack.push(19)
+myStack.push(7)
+console.log(myStack.peek()) // 7
+myStack.pop()
+myStack.print() // 5 19
