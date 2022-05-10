f8114be42818163c0123d2b75397dd0ca0c57d90
Abdelrahman Mostafa <Abdelraahmanmostafa@gmail.com>
2022-05-10 17:32:25 +0200

    Create BinarySearch.js

---
+++ BinarySearch.js
@@ -0,0 +1,14 @@
+function binarySearch (arr, x) { 
+	let start=0;
+	let end=arr.length-1;
+	while (start<=end){
+		let mid=Math.floor((start + end)/2);
+		if (arr[mid]===x) return mid;
+		else if (arr[mid] < x)
+			start = mid + 1;
+		else
+			end = mid - 1;
+	}
+	return -1;
+}
+console.log(binarySearch([1,4,2,5,6,7],6)) // 4
