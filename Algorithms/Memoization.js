5886365993245dfcd8ec26624c679df250d9bf4c
levvw <abdelraahmanmostafa@gmail.com>
2022-05-14 22:28:56 +0200

    typo

--- Algorithms/Memoization.js
+++ Algorithms/Memoization.js
@@ -1,15 +1,15 @@
 function addTo69() {
-    const cache = {}; 
-    // useing closures to avaid object being initiliazed every run
-    return function(n) {
-        if (cache[n]===undefined) {
-             console.log("this runs once")
-             cache[n] = n + 69 // could be more complex
-              }
-    return cache[n] // o(1)
-   }
+  const cache = {};
+  // useing closures to avoid object being initiliazed every run
+  return function (n) {
+    if (cache[n] === undefined) {
+      console.log("this runs once");
+      cache[n] = n + 69; // could be more complex
+    }
+    return cache[n]; // o(1)
+  };
 }
-const adder69 = addTo69()
-console.log(adder69(1))
-console.log(adder69(1))
-console.log(adder69(1))
+const adder69 = addTo69();
+console.log(adder69(1));
+console.log(adder69(1));
+console.log(adder69(1));
